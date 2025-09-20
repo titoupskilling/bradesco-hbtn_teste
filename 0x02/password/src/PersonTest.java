835b94e4312855fb61df0d4ba6844856770b3418
Renato <ren.tito@yahoo.com.br>
2025-09-20 14:46:41 +0000

    project estrutura de dados

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.ValueSource;
 
 public class PersonTest {
 
